d95b0b0026c0ba7a6badeb84d0328a0629023538
harshil <harshilgohel723@gmail.com>
2019-02-27 15:51:03 +0530

    added how to register

--- app/src/main/java/com/example/brizingr2k19/WebView.java
+++ app/src/main/java/com/example/brizingr2k19/WebView.java
@@ -1,19 +1,59 @@
 package com.example.brizingr2k19;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebViewClient;
 
 public class WebView extends AppCompatActivity {
 
+    private Toolbar mTopToolbar;
    android.webkit.WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
 
+        mTopToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mTopToolbar);
+        getSupportActionBar().setTitle("Register");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
         webView = findViewById(R.id.webView);
-        webView.loadUrl("https://www.google.co.in/");
+        webView.loadUrl("https://brizingr.gecrajkot.ac.in/");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.pdfAction) {
+            startActivity(new Intent(getApplicationContext(),RegisterView.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
